d66e4735172d4265a410cb3ccc0f24fa9d3e8847
Pranav Nag <153430540+PranavNag2005@users.noreply.github.com>
2025-07-30 18:12:12 +0530

    using Buitlin methods

---
+++ Linkedlist.java
@@ -0,0 +1,14 @@
+package twentynine;
+import java.util.*;
+public class Linkedlist {
+    public static void main(String[] args) {
+        LinkedList<Integer>ll=new LinkedList<>();
+        //adding linkedlist element 
+        ll.add(23);
+        //adding element at particualr index
+        ll.add(0, 12);
+        // displaying the linkedlist
+        System.out.println(ll);
+        
+    }
+}
