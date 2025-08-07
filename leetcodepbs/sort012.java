e925523aceaf824bae55e9ce9da23f961a1a3a39
Pranav Nag <153430540+PranavNag2005@users.noreply.github.com>
2025-08-07 18:46:23 +0530

    Create sort012.java

---
+++ leetcodepbs/sort012.java
@@ -0,0 +1,33 @@
+package ONE;
+
+public class Swap {
+    public static void swap(int arr[]){
+        int low=0;int mid=0;
+        int high=arr.length-1;
+        while(mid<=high){
+            if(arr[mid]==0){
+                int temp=arr[low];
+                arr[low]=arr[mid];
+                arr[mid]=temp;
+                low++;
+                mid++;
+            }
+            else if(arr[mid]==1){
+                mid++;
+            }else{
+                int temp=arr[mid];
+                arr[mid]=arr[high];
+                arr[high]=temp;
+                high-=1;
+                
+            }
+        }
+    }
+    public static void main(String[] args) {
+        int arr[]={0, 1, 2, 0, 1, 2};
+        swap(arr);
+        for(int a:arr){
+            System.out.println(a);
+        }
+    }
+}
