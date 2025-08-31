5b5802caabf5daca70f93a88f1ab5e70ba7ac9f3
Pranav Nag <153430540+PranavNag2005@users.noreply.github.com>
2025-08-31 12:06:52 +0530

    Create pb2942.java

---
+++ leetcodepbs/pb2942.java
@@ -0,0 +1,15 @@
+class Solution {
+    public List<Integer> findWordsContaining(String[] words, char x) {
+        
+        List<Integer>ans=new ArrayList<>();
+        int index=0;
+        for(String word:words){
+            if(word.contains(String.valueOf(x))){
+                ans.add(index);
+                
+            }
+            index+=1;
+        }
+        return ans;
+    }
+}
