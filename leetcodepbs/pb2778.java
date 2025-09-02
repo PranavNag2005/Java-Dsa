07fea73458a8427e6c9798617e4cea265ef739ed
Pranav Nag <153430540+PranavNag2005@users.noreply.github.com>
2025-09-02 11:23:27 +0530

    Create pb2778.java

---
+++ leetcodepbs/pb2778.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int sumOfSquares(int[] nums) {
+        int ans=0;
+        int n=nums.length;
+        for(int i=0;i<nums.length;i++){
+            if(n%(i+1)==0){
+
+                ans+=nums[i]*nums[i];
+            }
+        }
+        return ans;
+    }
+}
